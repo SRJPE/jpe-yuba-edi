d73979ee3d86ed353f4ae5ac812e1a5076b71515
InigoP <inigop@rogers.com>
2024-01-31 15:01:56 -0800

    fixing data conflict

--- data-raw/make_metadata_xml.R
+++ data-raw/make_metadata_xml.R
@@ -29,8 +29,6 @@ names(metadata) <- sheets
 
 abstract_docx <- "data-raw/metadata/abstract.docx"
 methods_docx <- "data-raw/metadata/methods.docx"
-#methods_docx <- "data-raw/metadata/methods.md" # use md for bulleted formatting. I don't believe lists are allowed in methods (https://edirepository.org/news/news-20210430.00)
-#update metadata
 catch_df <- readr::read_csv("data/yuba_catch.csv")
 catch_coverage <- tail(catch_df$visitTime, 1)
 metadata$coverage$end_date <- lubridate::floor_date(catch_coverage, unit = "days")
@@ -41,8 +39,6 @@ for (sheet_name in names(metadata)) {
   openxlsx::writeData(wb, sheet = sheet_name, x = metadata[[sheet_name]], rowNames = FALSE)
 }
 openxlsx::saveWorkbook(wb, file = excel_path, overwrite=TRUE)
-# edi_number <- reserve_id(user_id = Sys.getenv("edi_user_id"), Sys.getenv("edi_password"))
-# edi_number <- "edi.1529.1" # reserved 11-8-2023
 
 vl <- readr::read_csv("data-raw/version_log.csv", col_types = c('c', "D"))
 previous_number <- tail(vl['edi_version'], n=1)
@@ -101,34 +97,3 @@ EMLaide::update_package(user_id = secret_username,
                             eml_file_path = paste0(getwd(), "/", current_number, ".xml"),
                             existing_package_identifier = paste0("edi.",previous_id, ".", previous_ver, ".xml"),
                             environment = "staging")
-# EML::eml_validate(paste0(edi_number, ".xml"))
-
-# EMLaide::evaluate_package(Sys.getenv("edi_user_id"), Sys.getenv("edi_password"), paste0(edi_number, ".xml"))
-# EMLaide::upload_package(Sys.getenv("edi_user_id"), Sys.getenv("edi_password"), paste0(edi_number, ".xml"))
-
-# The code below is for updating the eml number and will need to be implemented when
-# we move to automated updates
-# doc <- read_xml(paste0(edi_number, ".xml"))
-# edi_number<- data.frame(edi_number = doc %>% xml_attr("packageId"))
-# update_number <- edi_number %>%
-#   separate(edi_number, c("edi","package","version"), "\\.") %>%
-#   mutate(version = as.numeric(version) + 1)
-# edi_number <- paste0(update_number$edi, ".", update_number$package, ".", update_number$version)
-
-# preview_coverage <- function(dataset) {
-#   coords <- dataset$coverage$geographicCoverage$boundingCoordinates
-#   north <- coords$northBoundingCoordinate
-#   south <- coords$southBoundingCoordinate
-#   east <- coords$eastBoundingCoordinate
-#   west <- coords$westBoundingCoordinate
-#
-#   leaflet::leaflet() |>
-#     leaflet::addTiles() |>
-#     leaflet::addRectangles(
-#       lng1 = west, lat1 = south,
-#       lng2 = east, lat2 = north,
-#       fillColor = "blue"
-#     )
-# }
-#
-# preview_coverage(dataset)
