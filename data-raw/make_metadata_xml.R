860cbe26aa376a9d7eb7e00d8b61eef11107583f
InigoP <ipeng@flowwest.com>
2025-01-02 11:27:35 -0800

    test with staging env

--- data-raw/make_metadata_xml.R
+++ data-raw/make_metadata_xml.R
@@ -109,5 +109,5 @@ EMLaide::update_edi_package(user_id = secret_edi_username,
                             password = secret_edi_password,
                             eml_file_path = paste0(getwd(), "/", current_edi_number, ".xml"),
                             existing_package_identifier = paste0("edi.",previous_edi_id, ".", previous_edi_ver, ".xml"),
-                            environment = "production")
+                            environment = "staging")
 
